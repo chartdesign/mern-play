e53c21cc92c08f93fc85ddf2d8bebeab2a5cb846
chartdesign <chartshorne@gmail.com>
2023-10-29 20:48:46 -0700

    changed server file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -14,7 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("API running"));
+// app.get("/", (req, res) => res.send("API running"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
